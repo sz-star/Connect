ac9c8e1f1cb9238f24ab922e8f3db290b9a7194b
zhuhuaqiang <616928019@qq.com>
2018-11-18 19:52:37 +0800

    :construction: 自带错误处理器默认返回500
    Signed-off-by: zhuhuaqiang <616928019@qq.com>

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const connect = require('connect');
 const setup = require('./logger.js')
  
 function hello(req, res) {
+  // foo();   此函数假如不存在,connect默认的处理方式是返回响应状态码500
   res.setHeader('Content-Type', 'text/plain');
   res.end('hello world');
 }
