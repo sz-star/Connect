037ed17f15430f8fc67400c53635542dc5e355ff
zhuhuaqiang <616928019@qq.com>
2018-11-18 19:46:30 +0800

    :chart_with_upwards_trend: 可配置的connect中间件logger
    Signed-off-by: zhuhuaqiang <616928019@qq.com>

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const connect = require('connect');
-
-function logger(req, res, next) {
-  console.log('%s %s', req.method, req.url);
-  next();
-}
-
+const setup = require('./logger.js')
+ 
 function hello(req, res) {
   res.setHeader('Content-Type', 'text/plain');
-  res.end('hello connect');
+  res.end('hello world');
 }
 
-connect()
-  .use(logger)
+const app = connect()
+  .use(setup(':method :url'))
   .use(hello)
   .listen(3000);
